13523bcf6270c731bfc70a1f5010da176412c684
rhordiichuk <rhordiichuk@gmail.com>
2025-12-27 20:02:33 +0100

    docs: remove Home Component JSDoc comment

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,12 +2,6 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { WidgetLibrary } from 'widget-library';
 
-/**
- * Home Component
- *
- * Landing page for the widget library demo application.
- * Displays the widget library and provides navigation to other features.
- */
 @Component({
   selector: 'app-home',
   imports: [WidgetLibrary, RouterLink],
